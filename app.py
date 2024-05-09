6b5f64510729cc74e31ee9d439965ea8a8c18e57
angorafish <mimitunafish@gmail.com>
2024-05-09 14:19:53 -0700

    Final commit

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-"""Flask app for Cupcakes"""
+from flask import Flask
+from models import db, Cupcake, connect_db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cupcakes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+connect_db(app)
--- models.py
+++ models.py
@@ -1,1 +1,15 @@
-"""Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Cupcake(db.Model):
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default='https://tinyurl.com/demo-cupcake')
+                      
+def connect_db(app):
+    """Connect the database to the Flask app."""
